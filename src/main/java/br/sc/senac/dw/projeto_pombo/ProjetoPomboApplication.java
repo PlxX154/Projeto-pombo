9fab2edd9dffcba2d183d58c1924e5fa37186891
KEVIN ANDERSON MACHADO RAULINO <kevin.raulino@alunos.sc.senac.br>
2024-08-27 10:27:32 -0300

    first commit

---
+++ src/main/java/br/sc/senac/dw/projeto_pombo/ProjetoPomboApplication.java
@@ -0,0 +1,13 @@
+package br.sc.senac.dw.projeto_pombo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPomboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPomboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/sc/senac/dw/projeto_pombo/ProjetoPomboApplicationTests.java
@@ -0,0 +1,13 @@
+package br.sc.senac.dw.projeto_pombo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPomboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
